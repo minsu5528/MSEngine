9614140296b77e05e8c9de7c9de01ebd4e5641ea
minsu5528@gmail.com <minsu5528@gmail.com>
2026-07-11 21:33:33 +0900

    feat: create vertex buffer for triangle rendering

--- MSEngine/main.cpp
+++ MSEngine/main.cpp
@@ -95,7 +95,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
 
     ComPtr<ID3D11RenderTargetView> renderTargetView;
     device->CreateRenderTargetView(backBuffer.Get(), nullptr, &renderTargetView);
-    /*
+    
     ComPtr <ID3D11Buffer> vertexBuffer;
 
     struct Vertex {
@@ -117,7 +117,7 @@ int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine
     initData.pSysMem = vertices;
 
     device->CreateBuffer(&bd, &initData, vertexBuffer.GetAddressOf());
-    */
+    
     float clearColor[4] = { 0.0f, 0.0f, 1.0f, 1.0f };  // R, G, B, A — 파란색
     deviceContext->ClearRenderTargetView(renderTargetView.Get(), clearColor);
     swapChain->Present(1, 0);
